a0e0b4674905e25c05391e4b0191c94fa6d78acf
Stuart Seguin <stuseguin@gmail.com>
2021-06-13 21:25:08 -0600

    chore: remove ui from docker-compose

---
+++ networthtracker-ui/src/app-container.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@material-ui/core";
+
+import React from "react";
+
+export const AppContainer = () => {
+  console.log("cow");
+  return (
+    <div>
+      <Button>Test</Button>
+      <p>Cow</p>
+    </div>
+  );
+};
